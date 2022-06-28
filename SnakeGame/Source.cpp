a2d5641579b023868cb5da6f51505e3c6b4da925
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-06-28 22:18:57 +0400

    Create Draw() function
    Add Draw, and game Fild

--- SnakeGame/Source.cpp
+++ SnakeGame/Source.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
 #include <conio.h>
+using namespace std;
 
-int main()
+bool GameOver;
+const int width = 20, height = 20;
+
+int x, y, FruitX, FruitY, score;
+
+int TailX[width * height];
+int TailY[width * height];
+int TailSize;
+
+enum Direction { STOP = 0, UP, DOWN, LEFT, RIGHT };
+Direction dir;
+
+void Setup()
+{
+    GameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+
+    FruitX = rand() % width;
+    FruitY = rand() % height;
+    score = 0;
+}
+void Draw()
+{
+    system("cls");
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0 || j == width - 1)
+            {
+                cout << "#";    
+            }   
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+
+    cout << endl;
+
+}
+void Input()
 {
 
+}
+void Logic()
+{
+
+}
+int main()
+{
+    Setup();
+    while (!GameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+    Setup();
     return 1;
 }
